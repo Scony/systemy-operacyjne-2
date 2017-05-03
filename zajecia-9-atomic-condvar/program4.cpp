e6449c738650a013832628f7f11c1135694bfff7
Pawel Lampe <pawel.lampe@gmail.com>
2017-05-03 17:11:21 +0200

    condvars

---
+++ zajecia-9-atomic-condvar/program4.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+int glob = 0;
+std::mutex mx;
+std::condition_variable cv;
+
+void foo()
+{
+  std::cout << "foo sleeping..." << std::endl;
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+
+  std::cout << "foo waiting..." << std::endl;
+  std::unique_lock<std::mutex> lock(mx);
+  cv.wait(lock);
+
+  std::cout << "foo done, glob: " << glob << std::endl;
+}
+
+void bar()
+{
+  std::cout << "bar sleeping..." << std::endl;
+  std::this_thread::sleep_for(std::chrono::seconds(4));
+
+  std::cout << "bar notifying..." << std::endl;
+  glob = 1337;
+  cv.notify_one();
+
+  std::cout << "bar done" << std::endl;
+}
+
+int main()
+{
+  std::cout << "main starting threads..." << std::endl;
+
+  std::thread fooThread(foo);
+  std::thread barThread(bar);
+
+  fooThread.join();
+  barThread.join();
+
+  std::cout << "main done" << std::endl;
+
+  return 0;
+}
